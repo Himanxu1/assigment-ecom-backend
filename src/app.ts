e653da0b56858263ed25b588f31d2935b7af98ed
Himanxu1 <technophilic000@gmail.com>
2026-09-21 22:47:35 +0530

    server created

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+
+import { env } from "./config/env.js";
+
+export const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json({ limit: "100kb" }));
+
+if (env.NODE_ENV !== "test") {
+  app.use(morgan("dev"));
+}
+
+app.get("/health", (_req, res) => {
+  res.json({ status: "ok" });
+});
